737479d1e833bef87b76427faf30c4e62c6280f0
Jodi Croft <jodipruett@gmail.com>
2020-05-21 19:56:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 require "pry"
 
 def reverse_each_word(sentence)
-  sentence_array = [sentence]
+  sentence_array = []
+  sentence_array << sentence
   sentence_array.each do |word|
-    binding.pry
     word.reverse
   end
 end
